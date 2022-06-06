af6dc4c01e9c42eb1885289404ace5488d78fc90
wawangamer <53104381+wawangamer@users.noreply.github.com>
2022-06-06 12:09:45 +0700

    Update HandleMsg.js

--- HandleMsg.js
+++ HandleMsg.js
@@ -301,7 +301,7 @@ module.exports = HandleMsg = async (urbae, message) => {
 		const StickerMetadatacrop = { author: authorstc, pack: packstc, keepScale: false }
 
 		// [IDENTIFY]
-		const ownerNumber = "62895334951166@c.us"
+		const ownerNumber = "6283129544189@c.us"
 		const errorImg = "https://i.ibb.co/DYLd6fk/baukrysie.jpg"
 		const isOwnerBot = ownerNumber.includes(pengirim)
 		const isOwner = ownerNumber.includes(pengirim)
